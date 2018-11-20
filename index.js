20968053d519cd3174e8fb9593a08ab89b79cb40
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-20 03:44:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet(distance) {
   let result;
   if (distance <= 400) {
     result = 'This one is on me!';
-  } else if (distance < 2000) {
+  } else if (distance < 2000 && distance <= 2500) {
     result = 'I will gladly take your thirty bucks.';
   } else if (distance > 2500) {
     result = 'No can do.';
